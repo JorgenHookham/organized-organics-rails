6d39dc6278e1c686f34f3bc8a8ed582ba4adf99e
Jørgen <jorgen@hookham.co>
2014-02-07 16:19:46 -0800

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OrganizedOrganics::Application.config.secret_key_base = 'c1cfebcf9b0eb421077da6ca4ee2fb9bafb2ea41b406c9c18ec427e2b70186ed6c9ae4642a0bb6359c72eb3539a16951ad37e2d731d76f2fb356816473f9b929'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OrganizedOrganics::Application.config.session_store :cookie_store, key: '_organized-organics_session'
